a2d8bae50e7c08e546752a1549897543d3548575
Ricky <rzh4321@gmail.com>
2023-06-02 17:53:28 -0400

    added humidity and feels like to results, change unit button is remembered

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const main = document.querySelector('.main');
 const search_button = document.querySelector('.search');
 const inp = search_button.previousElementSibling;
-const results = document.getElementById('results');
-
+const results = document.querySelector('.results');
+let unit = 'f';
 
 search_button.addEventListener('click', (e) => {
     e.preventDefault();
@@ -20,9 +20,6 @@ search_button.addEventListener('click', (e) => {
     }
 })
 
-
-
-
 async function get_data(inp) {
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=69ebdce0bd7c44ba98102952230106&q=${inp}`, {mode: 'cors'});
     const data = await response.json();
@@ -38,52 +35,65 @@ function display_data(data) {
     let feelslike_c = data.current.feelslike_c;
     let feelslike_f = data.current.feelslike_f;
     let humidity = data.current.humidity;
-    console.log([city, country])
     let temps = [temp_f, temp_c, feelslike_f, feelslike_c];
+    console.log([city, country])
 
-    let location = display_location(city, country);
-    let temp = create_temp_div(temps);
-    let humidity_div = create_humidity(humidity);
-    let feelslike_div = create_feelslike(feelslike_f);
-
-
-    results.append(location, temp, humidity_div, feelslike_div);
-}
-
-function create_feelslike(temp) {
-    let div = document.createElement('div');
-    div.id = 'feelslike';
-    div.textContent = `Feels like: ${temp}° F`;
-    return div;
-}
-
-function create_humidity(humidity) {
-    let div = document.createElement('div');
-    div.textContent = `Humidity: ${humidity}`;
-    return div;
-}
-
-function display_location(city, country) {
     let display_location = document.createElement('span');
     display_location.textContent = `${city}, ${country}`;
-    return display_location;
+
+    let temp = create_temp_div(temps);
+    let humidity_div = create_humidity_div(humidity);
+    let feelslike_div = create_feelslike_div(feelslike_f, feelslike_c);
+
+    results.append(display_location, temp, humidity_div, feelslike_div);
+
+}
+
+
+function create_feelslike_div(feelslike_f, feelslike_c) {
+    let span = document.createElement('span');
+    span.id = 'feelslike'
+    if (unit == 'f') {
+        span.textContent = `Feels like: ${feelslike_f}° F`;
+    }
+    else {
+        span.textContent = `Feels like: ${feelslike_f}° C`;
+    }
+    return span
+}
+
+
+function create_humidity_div(humidity) {
+    let div = document.createElement('div');
+    div.textContent = `Humidity: ${humidity}`;
+    return div
 }
 
 function create_temp_div(temps) {
+    let [temp_f, temp_c] = temps;
     let temp_div = document.createElement('div');
     let temp = document.createElement('span');
-    let [temp_f, temp_c] = temps
-    temp.textContent = `${temp_f}° F`;
+    temp.id = 'temp';
     let change_unit = document.createElement('button');
     change_unit.classList.add('change-unit');
-    change_unit.textContent = 'Show celsius';
+    
+    if (unit == 'f') {
+        temp.textContent = `${temp_f}° F`;
+        change_unit.textContent = 'Show celsius';
+    }
+    else {
+        temp.textContent = `${temp_c}° C`;
+        change_unit.textContent = 'Show farenheit';
+    }
 
     change_unit.addEventListener('click', (e) => {
-        if (change_unit.textContent == 'Show celsius') {
-            change_units(temp, change_unit, temps, 'c');
+        if (unit == 'f') {
+            unit = 'c'
+            change_units(temps);
         }
         else {
-            change_units(temp, change_unit, temps, 'f');
+            unit = 'f';
+            change_units(temps);        
         }
     })
 
@@ -93,22 +103,24 @@ function create_temp_div(temps) {
     return temp_div;
 }
 
-function change_units(temp, change_unit, temps, unit) {
+function change_units(temps) {
     let [temp_f, temp_c, feelslike_f, feelslike_c] = temps;
+    let temp = document.getElementById('temp');
+    let change_unit_btn = document.querySelector('.change-unit')
     let feelslike = document.getElementById('feelslike');
-    // change feels like
     if (unit == 'c') {
         temp.textContent = `${temp_c}° C`;
-        change_unit.textContent = 'Show farenheit';
-        feelslike.textContent = `Feels like: ${feelslike_c}° C`
+        change_unit_btn.textContent = 'Show farenheit';
+        feelslike.textContent = `Feels like: ${feelslike_c}° C`;
     }
     else {
         temp.textContent = `${temp_f}° F`;
-        change_unit.textContent = 'Show celsius';
-        feelslike.textContent = `Feels like: ${feelslike_f}° F`
-
+        change_unit_btn.textContent = 'Show celsius';
+        feelslike.textContent = `Feels like: ${feelslike_f}° F`;
     }
 }
+
+
 
 function check_validity() {
     const form = document.querySelector('form');
@@ -132,4 +144,10 @@ function display_no_matches(error_message) {
     msg.textContent = error_message;
     msg.style.color = 'red';
     results.append(msg);
+}
+
+function clear_results() {
+    for (let elem of results.children) {
+        elem.remove();
+    }
 }
